096edc68e8da224e87f9d2d03fa620ccc518643b
Imrajkumar29 <rajkumar291994@gmail.com>
2022-12-10 14:37:55 +0530

    movies app import export

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import img1 from './img/1.avif';
 import './App.css';
+import {useState} from "react";
+import {Movielist} from "./Movielist.js";
+import {Movie} from "./Movie.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movielist/>  
     </div>
+  );
+}
+
+//hooks
+function Counter(){
+  //const[t1,t2]=dbl(10);
+ 
+  let [like,setlike] =useState(0);
+  let [dislike,setdislike] =useState(0);
+  const likeStyle= {
+    color :like >=10?"green":"black",
+    //fontWeight:"bold",
+  } ;
+  const dislikeStyle= {
+    color :dislike >=10?"red":"black",
+    //fontWeight:"bold",
+  } ;
+  const messageStyle ={
+    display :like-dislike>=10 ?"block":"none",
+  };
+  return(
+<div>
+  {/* onClick ->camelCase */}
+  {/* conditional rendering -> this is preferred over conditional styling as this doesn't occupy dom*/}
+  {like -dislike >=10 ?<h2> you are awesome🎸</h2>:null}
+  {/* conditional styling */}
+  {/* <h2 style={messageStyle}> you are awesome🎸</h2> */}
+  <button style ={likeStyle} onClick={()=>setlike(like+1)}>like👍 {like}</button>
+  <button style ={dislikeStyle} onClick={()=>setdislike(dislike+1)}>dislike😏 {dislike}</button>
+</div>
   );
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,18 @@
+function Movie({movie}){
+    
+    const ratingstyle ={
+      color:movie.rating >=8.5 ?"green":"red",
+    }
+    return(
+      <div className ="movie-container">
+        <img src ={movie.poster} alt ={movie.name} className ="movie-poster"/>
+        <div className="movie-specs">
+      <h2 className="movie-name">{movie.name}</h2>
+      <p style ={ratingstyle} className ="movie-rating">⭐{movie.rating}</p>
+      </div>
+      <p className ="movie-summary">{movie.summary}</p>
+    </div>);
+  }
+  export{Movie};
+  
+  
