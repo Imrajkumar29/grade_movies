9fec478489a2efa0cdd9465f174ccba650844436
Imrajkumar29 <rajkumar291994@gmail.com>
2022-12-24 12:10:40 +0530

    added material ui changes

--- src/App.js
+++ src/App.js
@@ -12,34 +12,5 @@ function App() {
   );
 }
 
-//hooks
-function Counter(){
-  //const[t1,t2]=dbl(10);
- 
-  let [like,setlike] =useState(0);
-  let [dislike,setdislike] =useState(0);
-  const likeStyle= {
-    color :like >=10?"green":"black",
-    //fontWeight:"bold",
-  } ;
-  const dislikeStyle= {
-    color :dislike >=10?"red":"black",
-    //fontWeight:"bold",
-  } ;
-  const messageStyle ={
-    display :like-dislike>=10 ?"block":"none",
-  };
-  return(
-<div>
-  {/* onClick ->camelCase */}
-  {/* conditional rendering -> this is preferred over conditional styling as this doesn't occupy dom*/}
-  {like -dislike >=10 ?<h2> you are awesome🎸</h2>:null}
-  {/* conditional styling */}
-  {/* <h2 style={messageStyle}> you are awesome🎸</h2> */}
-  <button style ={likeStyle} onClick={()=>setlike(like+1)}>like👍 {like}</button>
-  <button style ={dislikeStyle} onClick={()=>setdislike(dislike+1)}>dislike😏 {dislike}</button>
-</div>
-  );
-}
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,11 @@
 import {Counter} from "./Counter.js";
+import {useState} from "react";
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+
 function Movie({movie}){
-    
+  let [show,setShow] =useState(0);
     const ratingstyle ={
       color:movie.rating >=8.5 ?"green":"red",
     }
@@ -8,10 +13,14 @@ function Movie({movie}){
       <div className ="movie-container">
         <img src ={movie.poster} alt ={movie.name} className ="movie-poster"/>
         <div className="movie-specs">
-      <h2 className="movie-name">{movie.name}</h2>
+      <h3 className="movie-name">{movie.name}
+      <IconButton onClick={()=>setShow(!show)} aria-label="Toggle summary" color="primary">
+        {show?<ExpandLessIcon/>:<ExpandMoreIcon/>}
+        </IconButton> 
+      {show?<p className ="movie-summary">{movie.summary}</p>:null}
+      </h3>
       <p style ={ratingstyle} className ="movie-rating">⭐{movie.rating}</p>
       </div>
-      <p className ="movie-summary">{movie.summary}</p>
       <Counter/>
     </div>
     );
