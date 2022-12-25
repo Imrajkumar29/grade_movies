55edcf70f7f80f0b51a37a41e807b154be2b7d37
Imrajkumar29 <rajkumar291994@gmail.com>
2022-12-25 18:30:36 +0530

    movies change

--- src/App.js
+++ src/App.js
@@ -3,14 +3,60 @@ import './App.css';
 import {useState} from "react";
 import {Movielist} from "./Movielist.js";
 import {Movie} from "./Movie.js";
+import {
+  Routes,
+  Route,
+Link} from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+//import { Home } from '@mui/icons-material';
 
 function App() {
   return (
-     <div className="App">
-      <Movielist/>  
+     <div className="App"> 
+
+    <ul>
+      <li>
+        <Link to="/"> Home</Link>
+      </li>
+      <li>
+        <Link to="/movies"> Movies</Link>
+      </li>
+    </ul>
+
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/movies" element={  <Movielist/> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes> 
     </div>
   );
 }
+//https://63a6dc12f8f3f6d4ab13a8d2.mockapi.io/movies
+function NotFound()
+{
+  return (
+  <div>
+    <h1>404 not found</h1>
+  </div>
+  );
+}
 
+function Home(){
+  return(
+    <div>
+      <h1>Welcome to the Movie app</h1>
+    </div>
+  );
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });  
+  return (
+    <ThemeProvider theme={darkTheme}>
+    </ThemeProvider>
+  );
+}
 
 export default App;
