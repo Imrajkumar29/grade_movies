a89b78654080fe7336d803ca5ba72b7342903399
Imrajkumar29 <rajkumar291994@gmail.com>
2022-12-31 04:18:43 +0530

    added trailer

--- src/Movie.js
+++ src/Movie.js
@@ -3,12 +3,15 @@ import {useState} from "react";
 import IconButton from '@mui/material/IconButton';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import InfoIcon from '@mui/icons-material/Info';
+import { useNavigate } from "react-router-dom";
 
-function Movie({movie}){
+function Movie({movie,id}){
   let [show,setShow] =useState(0);
     const ratingstyle ={
       color:movie.rating >=8.5 ?"green":"red",
     }
+    const navigate =useNavigate();
     return(
       <div className ="movie-container">
         <img src ={movie.poster} alt ={movie.name} className ="movie-poster"/>
@@ -18,8 +21,11 @@ function Movie({movie}){
       </div>
        <IconButton onClick={()=>setShow(!show)} aria-label="Toggle summary" color="primary">
         {show?<ExpandLessIcon/>:<ExpandMoreIcon/>}
-        </IconButton> 
-      {show?<p className ="movie-summary">{movie.summary}</p>:null}     
+        </IconButton>
+      {show?<p className ="movie-summary">{movie.summary}</p>:null} 
+      <IconButton onClick={()=>navigate(`/movies/${id}`)} aria-label="" color="primary">
+        <InfoIcon/> -{id}
+        </IconButton>  
       <Counter/>
     </div>
     );
